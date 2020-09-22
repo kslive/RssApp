ee518065c1a8853e83c4e67e648428a05340bf1d
ksllive <zhenya-kiselev-97@mail.ru>
2020-09-22 15:02:36 +0300

    Initial Commit

---
+++ RssApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RssApp
+//
+//  Created by Eugene Kiselev on 22.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
